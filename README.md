f74993aa35e2ca481aec383317a78b867f30a589
Thomas Goodwin <thomas@goodiemania.org>
2021-05-09 21:49:04 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MapTest
+
+This is to show, and ask for help with a problem I am having involving Leaflet JS
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {circle, LatLngExpression, Map, map, marker} from 'leaflet';
+
+
+@Component({
+  selector: 'app-map',
+  template: '<div id="map" style="height: 70em; width: 70em"></div>',
+  styleUrls: [
+    './map.component.css',
+    '../../../node_modules/leaflet/dist/leaflet.css'],
+})
+export class MapComponent implements OnInit {
+  private map!: Map;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.map = map('map', {
+      attributionControl: false,
+      zoomControl: false,
+    });
+
+    this.createMarkers({lat: 0, lng: 0})
+    this.createMarkers({lat: 5, lng: 0})
+    this.createMarkers({lat: -5, lng: 0})
+    this.createMarkers({lat: 0, lng: 5})
+    this.createMarkers({lat: 0, lng: -5})
+
+    this.map.setView([0, 0], 4);
+  }
+
+  private createMarkers(latLang: LatLngExpression) {
+    marker(latLang).addTo(this.map);
+    circle(latLang, {radius: 800, color: 'red'}).addTo(this.map);
+  }
+}
